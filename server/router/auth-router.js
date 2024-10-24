6b4b44d77082dd606e44a5120c7cc40b763a5425
Dhritiman Saikia <147172527+dhritiman78@users.noreply.github.com>
2024-10-24 23:47:03 +0530

    Delete server directory

--- server/router/auth-router.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const authcontrollers = require('../controllers/auth-controller')
-router.route('/login').post(authcontrollers.login)
-router.route('/registration').post(authcontrollers.registration)
-
-// Export the router
-module.exports = router
